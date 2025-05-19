4187d5b6b222eef4b0fa404f36beca424443bb55
Lukas Mader <spioggater@gmail.com>
2025-05-20 01:05:05 +0200

    Initial Commit

---
+++ iza-app/iza_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  iza_appApp.swift
+//  iza-app
+//
+//  Created by Lukáš Mader on 20/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct iza_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
